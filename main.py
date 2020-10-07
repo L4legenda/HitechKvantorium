30fc33450d544aca3ed6a606d949f9a15738f821
Alexandr <l4legenda@gmail.com>
2020-10-07 10:45:38 +0500

    copy elements

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from openpyxl import load_workbook, drawing
+from openpyxl import load_workbook, drawing, Workbook
 import qrcode
 
 wb_form = load_workbook("data/data.xlsx")
@@ -21,65 +21,41 @@ qr.make(fit=True)
 img = qr.make_image(fill_color="black", back_color="white")
 img.save("qr/qrcode.png")
 
-wb_form["Лист1"]["I7"]  = seria
-wb_form["Лист1"]["I23"] = seria
-wb_form["Лист1"]["I39"] = seria
-wb_form["Лист1"]["I55"] = seria
-wb_form["Лист1"]["Y7"]  = seria
-wb_form["Лист1"]["Y23"] = seria
-wb_form["Лист1"]["Y39"] = seria
-wb_form["Лист1"]["Y55"] = seria
-
-wb_form["Лист1"]["D10"] = seria
-wb_form["Лист1"]["D26"] = seria
-wb_form["Лист1"]["D42"] = seria
-wb_form["Лист1"]["D58"] = seria
-wb_form["Лист1"]["T10"] = seria
-wb_form["Лист1"]["T26"] = seria
-wb_form["Лист1"]["T42"] = seria
-#Nums
-wb_form["Лист1"]["D2"] = i
-wb_form["Лист1"]["I9"] = i
-i += 1
-wb_form["Лист1"]["D18"] = i
-wb_form["Лист1"]["I25"] = i
-i += 1
-wb_form["Лист1"]["D34"] = i
-wb_form["Лист1"]["I41"] = i
-i += 1
-wb_form["Лист1"]["D50"] = i
-wb_form["Лист1"]["I57"] = i
-i += 1
-wb_form["Лист1"]["T2"] = i
-wb_form["Лист1"]["Y9"] = i
-i += 1
-wb_form["Лист1"]["T18"] = i
-wb_form["Лист1"]["Y25"] = i
-i += 1
-wb_form["Лист1"]["T34"] = i
-wb_form["Лист1"]["Y41"] = i
-i += 1
-wb_form["Лист1"]["T50"] = i
-wb_form["Лист1"]["Y57"] = i
-
-wb_form["Лист1"]["E2"] = money
-wb_form["Лист1"]["E18"] = money
-wb_form["Лист1"]["E34"] = money
-wb_form["Лист1"]["E50"] = money
-wb_form["Лист1"]["U2"] = money
-wb_form["Лист1"]["U18"] = money
-wb_form["Лист1"]["U34"] = money
-wb_form["Лист1"]["U50"] = money
-
+list1 = wb_form["Лист1"]
+cols = "ABCDEFGHIJKLMNO"
 ws = wb_form.active
-my_png = drawing.image.Image("qr/qrcode.png")
-ws.add_image(my_png, "K3")
-ws.add_image(my_png, "K19")
-ws.add_image(my_png, "K35")
-ws.add_image(my_png, "K51")
-ws.add_image(my_png, "AA3")
-ws.add_image(my_png, "AA19")
-ws.add_image(my_png, "AA35")
-ws.add_image(my_png, "AA51")
+
+lists = {}
+
+for c in cols:
+    for r in range(1, 17):
+        val = None
+        style = None
+
+        try:
+            val = ws[c + str(r)].value
+            style = ws[c + str(r)].style
+        except:
+            print(c, r, "Не доступна")
+
+        lists[c + str(r)] = {
+            "val": val,
+            "style": style
+        }
+print(lists)
+
+for l in lists:
+    id = l[0] + str( int(l[1:]) + 16 )
+    try:
+        wb_form["Лист1"][id] = lists[l]["val"]
+        list1[id] = lists[l]["val"]
+    except:
+        print(id, "Ошибка")
+print("Good")
+
+#
+#my_png = drawing.image.Image("qr/qrcode.png")
+#ws.add_image(my_png, "K3")
+
 
 wb_form.save("export.xlsx")
